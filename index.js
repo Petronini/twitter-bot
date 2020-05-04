2b998a895367628241011119e71a88e15c5bc794
Petronini <jordi.or@gmail.com>
2020-05-04 17:29:50 +0200

    credenciales de twitter

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 const Twit = require("twit"); //Importamos la libreria de twit 2.2.11
+var config = require('./config.js'); //Importamos la configuracion de las 
+                                    //credenciales de twitter desde el archivo config.js
+
+//Configuramos la API de Twitter, los datos estan en el .env
+var T = new Twit(config);
