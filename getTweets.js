cd6f936d8363de4fcd250e446d7911a02d2b6648
Petronini <jordi.or@gmail.com>
2020-05-05 22:04:25 +0200

    keeping progress

---
+++ getTweets.js
@@ -0,0 +1,22 @@
+const Twit = require("twit");   // módulos necesarios
+var config = require('./config.js');
+
+//configuración API's Twitter
+var T = new Twit(config);
+// Barcelona 41.3897, 2.1568
+const loc = '41.3897,2.1568,20km';
+T.get('search/tweets', { 
+  q: '#covid19 OR #quedateencasa since:7 days', 
+  geocode: loc, 
+  language: 'cat', 
+  count: 100 }, gotData);
+
+function gotData(err, data) {
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+    console.log(
+      tweets[i].created_at,
+      tweets[i].user.screen_name,
+      tweets[i].user.location);
+  }
+}
