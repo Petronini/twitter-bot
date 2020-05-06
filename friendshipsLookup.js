d6e38ced8c8c305b05c0eb53b43ad2c7e9325679
Petronini <jordi.or@gmail.com>
2020-05-06 11:08:15 +0200

    Get friendships/lookup

---
+++ friendshipsLookup.js
@@ -0,0 +1,32 @@
+const Twit = require('twit');
+const config = require('./config.js');
+// API's Twitter config
+let T = new Twit(config);
+/////////////////////////
+
+// let stream = T.stream('user'); // user stream
+
+// console.log('listening...'); 
+// stream.on('follow', followed);
+
+// function followed(event) {
+//   console.log('I was followed by' + event.source.name + ' | ' + event.source.screen_name );
+  
+// }
+
+//
+// I only want to see tweets about my favorite fruits
+//
+ 
+// same result as doing { track: 'bananas,oranges,strawberries' }
+// var stream = T.stream('statuses/filter', { track: [ 'leñes', 'cojones' ] })
+// var stream = T.stream('followers/ids', {count: 5})
+ 
+// stream.on('follow', t => console.log(t)  )
+const path = 'friendships/lookup'; 
+const params = {screen_name: 'JorgeRoot, fvazquezrig, MezaJuanm, enfinpuesvale, oscardiazdliano'};  
+T.get( path, params, (err, data, response) => {
+    // console.log(data.users[1].name)
+    //data.forEach( element => console.log(element.id_str ,element.screen_name) );
+    data.forEach( element => console.log(element));
+  } );
--- friendshipsShow.js
+++ friendshipsShow.js
@@ -5,12 +5,11 @@ let T = new Twit(config);
 /////////////////////////
 
 const path = 'friendships/show'; 
-const params = {source_screen_name: 'HansPeterT', target_screen_name: 'JorgeRoot' }; 
+const params = {source_screen_name: 'JorgeRoot', target_screen_name: 'Neo_end' }; 
 T.get( path, params, (err, data, response) => {
     // console.log(data.users[1].name)
     //data.users.forEach( element => console.log(element.name));
-    console.log( params.source_screen_name, data.relationship.source.following,
-                 params.target_screen_name ,data.relationship.target.following);
+    console.log( 'source: ' + params.source_screen_name, data.relationship.source.following,
+                 'target: ' + params.target_screen_name ,data.relationship.target.following);
     
   } );
-
